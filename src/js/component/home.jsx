7ac1765063724fbafadd07b14df90b480f384856
dariel0514 <dariel0514@hotmail.com>
2022-10-05 07:10:21 +0000

    almost done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+const [tasks, setTasks] = useState([])
+const [input, setInput] = useState("")
+const handleSubmit = (event) => {
+	event.preventDefault()
+	if (input != ""){
+		let addTask = {
+		id: Math.floor(Math.random() * 1000),
+		text: input,
+		completed: false 
+	}
+}
 };
+
+const deleteTasks = (id) => {
+	let filteredTask = tasks.filter( task => task.id !== id );
+	setTasks(filteredTask)
+}
+
+return(
+	<div className="text-center">
+		<h1 className="todo">todos</h1>
+		<div className="list-card">
+			<form onSubmit={handleSubmit}>
+				<input
+					type="text"
+					value={input}
+					onChange={event => setInput(event.target.value)}
+					className="input-box"
+					placeHolder="What needs to be done?"
+					/>
+			</form>
+	<div className="list-items">
+		{tasks.map((task)=>{
+			<div className="todo" key={task.id}>
+				<p>{task.text}
+				<button
+				className="button"
+				onClick={ () => deleteTask(task.id) }>
+					&#10060;
+				</button>
+				</p>
+			</div>
+})}
+		<p className="counter">{ tasks.leng === 1 ? "one task left" : `tasks left: ${tasks.length}`}</p>
+	</div>
+	</div>
+	</div>
+);
+};
+
 
 export default Home;
